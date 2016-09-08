a6df3f01893872b478a80a4a42fb046f8d2d80bc
Gary Stevens <gary.stevens@work-club.com>
2016-09-08 14:47:01 +0100

    brew_cask.sh commands tested and running correctly

--- install/brew_cask.sh
+++ install/brew_cask.sh
@@ -1,21 +1,18 @@
 # Install Caskroom
 brew tap caskroom/cask
-brew install brew-cask
+brew install brew-cask-completion
 brew tap caskroom/versions
 
 # Install packages
 
 apps=(
-  alfred
   charles
   cyberduck
   firefox
   google-chrome
   mou
-  opera
   postman
   sequel-pro
-  screenflow
   slack
   sourcetree
   spotify
@@ -25,4 +22,5 @@ apps=(
 brew cask install "${apps[@]}"
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
+# The following are tools by Sindre Sorhus. This line is excluded until I've tinkered with them in detail
+#brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
